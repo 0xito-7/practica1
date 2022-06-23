28a15c61c0ce3349d130e4e2a0484800f5ea7c8e
091910664 FREDY <fredy.bau96@gmail.com>
2022-06-23 11:52:52 -0500

    creacion de interfaz en ScrollView

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.practica1
 //enlazar elementos de la vista
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.practica1.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -15,6 +16,18 @@ class MainActivity : Activity() {
        //asignando un valor
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.imageButton3.setOnClickListener {
+           Toast.makeText(this,"Respoder",Toast.LENGTH_SHORT).show()
+
+        }
+
+        binding.imageButton4.setOnClickListener {//accion clic al botón
+            Toast.makeText(this,"Ingnorar",Toast.LENGTH_SHORT).show() //mensaje
+            System.exit(0) //cerrar la aplicacion
+        }
+
+
 
 
 
